43a953db9ecd20a50001010c5bace16cfa2d635a
Olaf Parfieńczyk <olaf.parfienczyk@pja.edu.pl>
2024-03-10 18:57:08 +0100

    Change array

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -4,7 +4,7 @@ Console.WriteLine("Hello, World! 3");
 
 static void Main(string[] args)
 {
-    int[] arr = { 1, 3, 2, 9, 1, 2 };
+    int[] arr = { 1, 3, 2, 7, 9, 1, 2, 5, 4 };
     Console.WriteLine("Average = " + Average(arr));
     Console.WriteLine("Max = " + Max(arr));
 }
