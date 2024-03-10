2f0b6d760ed6d0b03189eac2098b22d0f03946de
Olaf Parfieńczyk <olaf.parfienczyk@pja.edu.pl>
2024-03-10 18:50:58 +0100

    Average function

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
 Console.WriteLine("Hello, World! 3");
+
+static void Main(string[] args)
+{
+}
+
+static double Average(int[] ints)
+{ 
+    return ints.Sum() / (double) ints.Length;
+}
