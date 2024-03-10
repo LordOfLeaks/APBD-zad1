b5f3e7a5706892f6e324a2859ff86370a7d62157
Olaf Parfieńczyk <olaf.parfienczyk@pja.edu.pl>
2024-03-10 18:55:00 +0100

    Max function

--- Zadanie1/Program.cs
+++ Zadanie1/Program.cs
@@ -4,9 +4,17 @@ Console.WriteLine("Hello, World! 3");
 
 static void Main(string[] args)
 {
+    int[] arr = { 1, 3, 2, 9, 1, 2 };
+    Console.WriteLine("Average = " + Average(arr));
+    Console.WriteLine("Max = " + Max(arr));
 }
 
 static double Average(int[] ints)
-{ 
-    return ints.Sum() / (double) ints.Length;
+{
+    return ints.Sum() / (double)ints.Length;
+}
+
+static double Max(int[] ints)
+{
+    return ints.Max();
 }
